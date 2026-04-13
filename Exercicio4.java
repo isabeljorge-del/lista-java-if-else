5e98d22ff5dcfc217ee5053214d4b9f8dfa288d7
isabeljorge-del <isabeljorge@acad.ifma.edu.br>
2026-04-13 17:44:10 -0300

    Create Exercicio4.java

---
+++ Exercicio4.java
@@ -0,0 +1,13 @@
+public class Exercicio4 {
+
+    public static void main(String[] args) {
+
+        double nota = 7;
+
+        if (nota >= 7) {
+            System.out.println("Aprovado");
+        }
+
+    }
+
+}
