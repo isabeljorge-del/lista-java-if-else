e204675ccc52bf78178cad502c7ef813a2483801
isabeljorge-del <isabeljorge@acad.ifma.edu.br>
2026-04-13 17:45:45 -0300

    Create Exercicio7.java

---
+++ Exercicio7.java
@@ -0,0 +1,19 @@
+public class Exercicio7 {
+
+    public static void main(String[] args) {
+
+        int idade = 16;
+
+        if (idade <= 12) {
+            System.out.println("Criança");
+        } else if (idade <= 17) {
+            System.out.println("Adolescente");
+        } else if (idade <= 59) {
+            System.out.println("Adulto");
+        } else {
+            System.out.println("Idoso");
+        }
+
+    }
+
+}
