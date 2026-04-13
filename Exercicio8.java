c3a3457266b7072751a2b746b3f8001ffe15e87d
isabeljorge-del <isabeljorge@acad.ifma.edu.br>
2026-04-13 17:46:16 -0300

    Create Exercicio8.java

---
+++ Exercicio8.java
@@ -0,0 +1,17 @@
+public class Exercicio8 {
+
+    public static void main(String[] args) {
+
+        double valor = 75;
+
+        if (valor >= 100) {
+            System.out.println("Desconto de 10%");
+        } else if (valor >= 50) {
+            System.out.println("Desconto de 5%");
+        } else {
+            System.out.println("Sem desconto");
+        }
+
+    }
+
+}
