75ece8ccbb24c76623acaf19aa1ba30bce6267fe
isabeljorge-del <isabeljorge@acad.ifma.edu.br>
2026-04-13 17:44:41 -0300

    Create Exercicio5.java

---
+++ Exercicio5.java
@@ -0,0 +1,18 @@
+public class Exercicio5 {
+
+    public static void main(String[] args) {
+
+        String nome = "Luna";
+        double nota = 5.5;
+
+        if (nota >= 6) {
+            System.out.println(nome + " está aprovado");
+        } else if (nota >= 4) {
+            System.out.println(nome + " está de recuperação");
+        } else {
+            System.out.println(nome + " está reprovado");
+        }
+
+    }
+
+}
