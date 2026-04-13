46648d5bec93ac5762eb6f829aa655bb5d807ad6
isabeljorge-del <isabeljorge@acad.ifma.edu.br>
2026-04-13 17:42:45 -0300

    Create Exercicio2.java

---
+++ Exercicio2.java
@@ -0,0 +1,16 @@
+public class Exercicio2 {
+
+    public static void main(String[] args) {
+
+        int num1 = 15;
+        int num2 = 9;
+
+        if (num1 > num2) {
+            System.out.println("O maior número é: " + num1);
+        } else {
+            System.out.println("O maior número é: " + num2);
+        }
+
+    }
+
+}
