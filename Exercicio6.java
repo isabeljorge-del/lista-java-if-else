4bd83818261d1e3c302b0f3de010cb971916ec6b
isabeljorge-del <isabeljorge@acad.ifma.edu.br>
2026-04-13 17:45:14 -0300

    Create Exercicio6.java

---
+++ Exercicio6.java
@@ -0,0 +1,21 @@
+public class Exercicio6 {
+
+    public static void main(String[] args) {
+
+        int num1 = 10;
+        int num2 = 2;
+        char operacao = '*';
+
+        if (operacao == '+') {
+            System.out.println(num1 + num2);
+        } else if (operacao == '-') {
+            System.out.println(num1 - num2);
+        } else if (operacao == '*') {
+            System.out.println(num1 * num2);
+        } else if (operacao == '/') {
+            System.out.println(num1 / num2);
+        }
+
+    }
+
+}
